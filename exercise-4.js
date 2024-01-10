a05430c4ff6b580c232bb9a94700b7fa3bb78c9c
FiveTW87 <thanwalokwattanakul@gmail.com>
2024-01-10 14:18:54 +0700

    feat : complete EX

---
+++ exercise-4.js
@@ -0,0 +1,12 @@
+db.pizzaOrders.find({
+  quantity: { $gte: 5 },
+  credit_card_type: "mastercard",
+});
+db.pizzaOrders.find({
+  $and: [{ quantity: { $gte: 1 } }, { quantity: { $lte: 5 } }],
+  size: "small",
+});
+db.pizzaOrders.find({
+  quantity: { $gte: 10 },
+  credit_card_type: null,
+});
